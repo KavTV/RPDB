3cca86fddaff8321b6246c748447de6c5d1346a6
KavTV <KavTV@users.noreply.github.com>
2020-09-09 14:15:35 +0200

    small update to SQL and Library

--- Library/SKPDB-Library/SKPDB-Library/Dal.cs
+++ Library/SKPDB-Library/SKPDB-Library/Dal.cs
@@ -15,12 +15,15 @@ namespace SKPDB_Library
             this.connectionString = connectionString;
         }
 
-
+        /// <summary>
+        /// Returns a list with all students and their projects
+        /// </summary>
+        /// <returns></returns>
         public List<Student> GetStudents()
         {
             // Locals
             List<Student> studentList = new List<Student>();
-            
+
 
             // Execute function
             NpgsqlConnection connection = new NpgsqlConnection(connectionString);
@@ -38,13 +41,17 @@ namespace SKPDB_Library
                     reader["education"].ToString(),
                     reader["fullname"].ToString(),
                     GetStudentProjects(reader["username"].ToString())));
-                
+
             }
             connection.Close();
 
             return studentList;
         }
-
+        /// <summary>
+        /// Returns all projects that the student has
+        /// </summary>
+        /// <param name="username"></param>
+        /// <returns></returns>
         public List<MiniProject> GetStudentProjects(string username)
         {
             //Locals
@@ -62,7 +69,7 @@ namespace SKPDB_Library
             // Reads all content from reservations table
             while (reader.Read())
             {
-                
+
                 projectList.Add(new MiniProject(int.Parse(
                     reader["projectid"].ToString()),
                     reader["headline"].ToString(),
@@ -73,7 +80,10 @@ namespace SKPDB_Library
             connection.Close();
             return projectList;
         }
-
+        /// <summary>
+        /// Returns a list with all projects, and all students in the project
+        /// </summary>
+        /// <returns></returns>
         public List<Project> GetProjects()
         {
             //Locals
@@ -96,13 +106,17 @@ namespace SKPDB_Library
                     reader["headline"].ToString(),
                     reader["documentation"].ToString(),
                     reader["description"].ToString(),
-                    GetProjectStudents(projectId))) ;
+                    GetProjectStudents(projectId)));
 
             }
             connection.Close();
             return projectList;
         }
-
+        /// <summary>
+        /// Returns a list with all students in the project
+        /// </summary>
+        /// <param name="projectId">Id of project</param>
+        /// <returns></returns>
         public List<MiniStudent> GetProjectStudents(int projectId)
         {
             //Locals
@@ -125,14 +139,20 @@ namespace SKPDB_Library
                     reader["username"].ToString(),
                     reader["educationid"].ToString(),
                     reader["fullname"].ToString()));
-                
+
             }
             connection.Close();
             return studentList;
         }
-
+        /// <summary>
+        /// Creates a project
+        /// </summary>
         public void CreateProject(string headline, string documentation, string description, string username)
         {
+            if (string.IsNullOrWhiteSpace(headline) || string.IsNullOrWhiteSpace(username))
+            {
+                return;
+            }
 
             // Execute function
             NpgsqlConnection connection = new NpgsqlConnection(connectionString);
@@ -144,8 +164,12 @@ namespace SKPDB_Library
             command.Parameters.AddWithValue("username", username);
 
             ExecuteNonQuery(command);
-        }
 
+        }
+        /// <summary>
+        /// Deletes the project
+        /// </summary>
+        /// <param name="projectid">Id of project</param>
         public void DeleteProject(int projectid)
         {
 
@@ -156,8 +180,11 @@ namespace SKPDB_Library
             command.Parameters.AddWithValue("projectid", projectid);
 
             ExecuteNonQuery(command);
-        }
 
+        }
+        /// <summary>
+        /// Edits the whole project
+        /// </summary>
         public void EditProject(int projectid, string headline, string documentation, string description)
         {
 
@@ -171,19 +198,100 @@ namespace SKPDB_Library
             command.Parameters.AddWithValue("description", description);
 
             ExecuteNonQuery(command);
+
+        }
+
+        /// <summary>
+        /// Returns a single Student object
+        /// </summary>
+        /// <param name="username"></param>
+        /// <returns></returns>
+        public Student GetStudent(string username)
+        {
+            //Locals
+            Student student = null;
+
+            // Execute function
+            NpgsqlConnection connection = new NpgsqlConnection(connectionString);
+            NpgsqlCommand command = new NpgsqlCommand("SELECT * FROM fn_getstudent(@username)", connection);
+
+            command.Parameters.AddWithValue("username", username);
+
+            // Opens connection
+            connection.Open();
+            NpgsqlDataReader reader = command.ExecuteReader();
+
+            // Reads all content from reservations table
+            while (reader.Read())
+            {
+                string user = reader["username"].ToString();
+                student = new Student(
+                    user,
+                    reader["education"].ToString(),
+                    reader["fullname"].ToString(),
+                    GetStudentProjects(user));
+            }
+            connection.Close();
+
+            return student;
+        }
+
+        /// <summary>
+        /// Returns a single project object
+        /// </summary>
+        /// <param name="projectId">Id of the project</param>
+        /// <returns></returns>
+        public Project GetProject(string projectId)
+        {
+            //Locals
+            Project project = null;
+
+            // Execute function
+            NpgsqlConnection connection = new NpgsqlConnection(connectionString);
+            NpgsqlCommand command = new NpgsqlCommand("SELECT * FROM fn_getproject(@projectid)", connection);
+
+            command.Parameters.AddWithValue("projectid", projectId);
+
+            // Opens connection
+            connection.Open();
+            NpgsqlDataReader reader = command.ExecuteReader();
+
+            // Reads all content from reservations table
+            while (reader.Read())
+            {
+                int id = int.Parse(reader["username"].ToString());
+                project = new Project(
+                    id,
+                    reader["headline"].ToString(),
+                    reader["documentation"].ToString(),
+                    reader["description"].ToString(),
+                    GetProjectStudents(id));
+            }
+            connection.Close();
+
+            return project;
         }
 
 
         private void ExecuteNonQuery(NpgsqlCommand command)
         {
-            // Creates connection
-            NpgsqlConnection connection = new NpgsqlConnection(connectionString);
-            command.Connection = connection;
+            try
+            {
 
-            // Opens connections and writes to table
-            connection.Open();
-            command.ExecuteNonQuery();
-            connection.Close();
+                // Creates connection
+                NpgsqlConnection connection = new NpgsqlConnection(connectionString);
+                command.Connection = connection;
+
+                // Opens connections and writes to table
+                connection.Open();
+                command.ExecuteNonQuery();
+                connection.Close();
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
         }
     }
 }
--- Library/SKPDB-Library/SKPDB-Library/Manager.cs
+++ Library/SKPDB-Library/SKPDB-Library/Manager.cs
@@ -6,41 +6,76 @@ namespace SKPDB_Library
     public class Manager
     {
         Dal dal;
-        
+
 
         public Manager(string connectionString)
         {
             dal = new Dal(connectionString);
-            
+
         }
 
+        /// <summary>
+        /// Returns a list with all students and their projects
+        /// </summary>
+        /// <returns></returns>
         public List<Student> GetStudents()
         {
             return dal.GetStudents();
         }
-
+        /// <summary>
+        /// Returns a list with all projects, and all students in the project
+        /// </summary>
+        /// <returns></returns>
         public List<Project> GetProjects()
         {
             return dal.GetProjects();
         }
-
+        /// <summary>
+        /// Creates a project
+        /// </summary>
         public void CreateProject(string headline, string documentation, string description, string username)
         {
             dal.CreateProject(headline, documentation, description, username);
         }
-
+        /// <summary>
+        /// Deletes the project
+        /// </summary>
+        /// <param name="projectid">Id of project</param>
         public void DeleteProject(int id)
         {
             dal.DeleteProject(id);
         }
+        /// <summary>
+        /// Edits the whole project
+        /// </summary>
         public void EditProject(int projectid, string headline, string documentation, string description)
         {
             dal.EditProject(projectid, headline, documentation, description);
         }
-
+        /// <summary>
+        /// Returns a list with all students in the project
+        /// </summary>
+        /// <param name="projectId">Id of project</param>
         public List<MiniStudent> GetProjectStudents(int projectId)
         {
             return dal.GetProjectStudents(projectId);
+        }
+        /// <summary>
+        /// Returns a single Student object
+        /// </summary>
+        /// <param name="username"></param>
+        /// <returns></returns>
+        public Student GetStudent(string username)
+        {
+            return dal.GetStudent(username);
+        }
+        /// <summary>
+        /// Returns a single project object
+        /// </summary>
+        /// <param name="projectId">Id of the project</param>
+        public Project GetProject(string projectId)
+        {
+            return dal.GetProject(projectId);
         }
     }
 }
